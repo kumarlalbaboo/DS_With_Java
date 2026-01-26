c0f82fb5e6fe62a6dd8d056349005d413a6883f2
Abhimanyu <contact.lalbaboo@gmail.com>
2026-01-26 13:57:53 +0530

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 import LatestInterviewQuestions.Employee;
-import com.logical.program.basic.A;
 
 import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 
-public class Java8Code {
+public class Main {
 
     public static void main(String[] args) {
 
